5e61642a5797954a00278552018fe315f6c19b22
SamiraMicaela <micaelabava@hotmail.es>
2024-05-03 19:07:10 -0300

    primeros pasos

---
+++ src/movies.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+const BASE_URL = 'http://localhost:3030/movies';   
+
+@Injectable()
+export class MoviesService {
+    async getMovies(): Promise<movies[]>{
+        const res 
+    }
+  
+}
